1a7bc9fe8579613389ac3ac23fb3f9b77e6d8f42
Syed Zeeshan Haider <zeeshanhyf@gmail.com>
2020-04-05 12:09:47 +0200

    corrections

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,11 +5,11 @@ const VideoDetail = (props) => {
     return <div>Loading....</div>;
   }
 
-  const videoSrc = `https:www.youtube.com/embed/${props.video.id.videoId}`;
+  const videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}`;
   return (
     <div>
       <div className="ui embed">
-        <iframe title="Video Player" src={videoSrc} />
+        <iframe title="video player" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h3 className="ui header">{props.video.snippet.title}</h3>
